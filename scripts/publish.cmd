8edbe466acaca325284265a4850e836666bbdbba
MaulingMonkey <git@maulingmonkey.com>
2023-04-20 17:13:05 -0700

    scripts\publish.cmd: *.mjs, *.wasm are important too

--- scripts/publish.cmd
+++ scripts/publish.cmd
@@ -1,5 +1,8 @@
 @pushd "%~dp0.." && setlocal
 
+:: ensure build up-to-date
+cargo build --package maulingmonkey-chip8-website --lib --target=wasm32-wasi --release
+
 :: prep branch/worktree
 git remote add pages git@github.com:MaulingMonkey/chip8.git             2>NUL
 git fetch pages gh-pages                                                || goto :die
@@ -9,6 +12,8 @@ git worktree add .worktrees\pages pages/gh-pages                        || goto 
 cargo about generate about.hbs > .worktrees\pages\about.html            || goto :die
 cd .worktrees\pages
 copy /Y ..\..\crates\website\src\website.html   index.html              || goto :die
+copy /Y ..\..\target\wasm32-wasi\release\maulingmonkey_chip8_website.wasm website.wasm || goto :die
+robocopy ..\..\crates\website\src . *.mjs /NJH /NJS /NP
 mkdir examples                                                          2>NUL
 copy /Y ..\..\examples\sierpinski.ch8           examples\sierpinski.ch8 || goto :die
 
